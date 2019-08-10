dc264e5374bf2213dda8fc6b687cc0b0b3cabc55
Denis Yakorev <denisyakorev@mail.ru>
2019-08-10 12:26:34 +0300

    working on lib

--- WebTutorServer/wt/web/adaptTutor/adapt_lib.js
+++ WebTutorServer/wt/web/adaptTutor/adapt_lib.js
@@ -1,10 +1,11 @@
 /**
 * Функция распаковывает архив курса в папку курсов WebTutor
 * @param String zip_path - путь к архиву курса
+* @param String code - уникальный код курса
 * @return String - путь к каталогу с разархивированным курсом
 */
-function unzip_course(course_path){
-    var new_path = "x-local://wt/web/webtutor/"+String(DateToRawSeconds(Date()));
+function unzip_course(course_path, code){
+    var new_path = "x-local://wt/web/webtutor/"+ code;
     try{
         ZipExtract(course_path, new_path);
         return new_path;
@@ -19,31 +20,32 @@ function unzip_course(course_path){
  * @param String course_path - путь к каталогу курса
  * @return String - код курса
  */
-function get_code_from_path(course_path){    
+function get_code_from_path(course_path){
     if(StrMatchesPattern(course_path, '*\download.zip')){       
         var new_path = StrReplace(course_path, '\\download.zip', '');
+        new_path = StrReplace(new_path, '/download.zip', '');
         var path_length = StrLen(new_path);
-        var result = "";
-        for (var i = path_length; i = 1; i--){
-            if(StrCharRangePos(new_path, i, 1) == '\\'){
-                return SrtRightRangePos(new_path, i+1);
+        for (var i = path_length; i >= 1; i--){
+           if(StrCharRangePos(new_path, i, i+1) == '\\' || StrCharRangePos(new_path, i, i+1) == '/'){
+                return StrRightRangePos(new_path, i+1);
             }
         }        
     }else{
         return '';
-    }
-    
+    }    
 }
 
 
 /**
  * Получает из файла манифеста данные, необходимые для формирования модуля
  * @param Strind manifest_path - путь к файлу манифеста
- * @return Bool - флаг об успешности выполнения операции
+ * @return String - название курса
  * TODO
  */
 function get_module_data(manifest_path){
-    return false
+    alert(manifest_path);
+    var file = LoadFileData(manifest_path);
+    return StrScan(file, '%*s<title><![CDATA[%s]]></title>%*s')[0];
 }
 
 
@@ -55,7 +57,7 @@ function get_module_data(manifest_path){
 */
 function create_module(course_path){
     //return OpenNewDoc('x-local://wtv/wtv_course_module.xmd');
-    var manifest_path = course_path + "\\imsmanifest.xml";
+    var manifest_path = course_path + "/imsmanifest.xml";
     var module_data = get_module_data(manifest_path);
 
 }
